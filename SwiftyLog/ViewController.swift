35bdb9b35d1abb9b67d5a4279b83f5e037c6e1d7
Alex <therebeloper@gmail.com>
2019-04-28 01:08:16 +0300

    Initial Commit

---
+++ SwiftyLog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftyLog
+//
+//  Created by Alex Nagy on 28/04/2019.
+//  Copyright © 2019 Alex Nagy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
