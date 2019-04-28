5991c3068affb2bd8d358863a07ccdd9709220a9
Alex <therebeloper@gmail.com>
2019-04-28 03:35:42 +0300

    first commit

--- SwiftyLog/ViewController.swift
+++ SwiftyLog/ViewController.swift
@@ -13,6 +13,30 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        DispatchQueueHelper.delay(bySeconds: 4, dispatchLevel: .background) {
+            log.todo("\(Log.stats()) Fix this `delay` issue next week")/
+        }
+        
+        print("This is a simple print statement")
+        log.success("\(Log.stats()) Successfully launched app")/
+        
+        DispatchQueueHelper.delay(bySeconds: 1) {
+            log.warning("\(Log.stats()) Strike 1!")/
+        }
+        
+        DispatchQueueHelper.delay(bySeconds: 2) {
+            log.warning("\(Log.stats()) Strike 2!")/
+        }
+        
+        DispatchQueueHelper.delay(bySeconds: 3) {
+            log.error("\(Log.stats()) Strike 3! You're out!")/
+        }
+        
+        DispatchQueueHelper.delay(bySeconds: 5) {
+            log.ln("\(Log.stats()) Logging at its best")/
+        }
+        
     }
 
 
